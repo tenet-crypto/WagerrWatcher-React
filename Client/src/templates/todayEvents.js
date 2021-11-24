de3796e64d69be0cd150d7f53e0cfb5d3d350b28
tenet-crypto <tenet.crypto@gmail.com>
2021-11-23 21:36:54 -0600

    Added sort to status and total bet in Today's Events

--- Client/src/templates/todayEvents.js
+++ Client/src/templates/todayEvents.js
@@ -15,6 +15,13 @@ function TodayEvents(prop){
 	const [wgr_total, setWgr_total] = useState('Loading...');
 	const [usd_total, setUsd_total] = useState('Loading...');
 
+	const [activeSort, setActiveSort] = useState('bet');
+	const [arrow, setArrow] = useState(<svg pointerEvents="none" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-sort-numeric-up" viewBox="0 0 16 16">
+				  <path d="M12.438 1.668V7H11.39V2.684h-.051l-1.211.859v-.969l1.262-.906h1.046z"/>
+				  <path fillRule="evenodd" d="M11.36 14.098c-1.137 0-1.708-.657-1.762-1.278h1.004c.058.223.343.45.773.45.824 0 1.164-.829 1.133-1.856h-.059c-.148.39-.57.742-1.261.742-.91 0-1.72-.613-1.72-1.758 0-1.148.848-1.835 1.973-1.835 1.09 0 2.063.636 2.063 2.687 0 1.867-.723 2.848-2.145 2.848zm.062-2.735c.504 0 .933-.336.933-.972 0-.633-.398-1.008-.94-1.008-.52 0-.927.375-.927 1 0 .64.418.98.934.98z"/>
+				  <path d="M4.5 13.5a.5.5 0 0 1-1 0V3.707L2.354 4.854a.5.5 0 1 1-.708-.708l2-1.999.007-.007a.498.498 0 0 1 .7.006l2 2a.5.5 0 1 1-.707.708L4.5 3.707V13.5z"/>
+				</svg>);
+
 
 	useEffect(() =>{
 		if(events != false){
@@ -22,6 +29,48 @@ function TodayEvents(prop){
 		};
 
 	},[events]);
+
+	//sort Arrays
+	function array_sort(type, direction, data_in){
+		const sortBy = ['Open','Pending','Complete',];
+		console.log(`type ${type}, direction: ${direction}`);
+		//if data is
+		if(data_in != false){
+			//loop array
+			if(type == "bet"){
+				if(direction == "desc"){
+					return data_in.sort(function(a,b){
+						return Number(b.betValue) - Number(a.betValue);
+					});
+				}else{
+					return data_in.sort(function(a,b){
+						return Number(a.betValue) - Number(b.betValue);
+					});
+				}
+				
+			}
+
+			if(type == "status"){
+				if(direction == "desc"){
+					return data_in.sort(function(a,b){
+
+						return sortBy.indexOf(a.status) - sortBy.indexOf(b.status);
+		
+					});
+				}else{
+					return data_in.sort(function(a,b){
+
+						return sortBy.indexOf(b.status) - sortBy.indexOf(a.status);
+		
+					});
+					
+				}
+				
+			}
+		}else{
+			return false;
+		}
+	}
 
 	function sortToday(){
 
@@ -102,7 +151,7 @@ function TodayEvents(prop){
 
 	useEffect(() =>{
 		if(today != null){
-			mapToday(today);
+			mapToday(array_sort(activeSort,"desc",today));
 			mapTotals(today);
 		}
 
@@ -168,6 +217,39 @@ function TodayEvents(prop){
 
 	}
 
+	//sort click
+	function sort_click(e){
+		var test = e.target.tagName;
+		
+		var type = e.target.dataset.sort;
+		var direction = e.target.dataset.direction;
+		
+
+		if(direction == 'desc'){
+				mapToday( array_sort(type,"asc",today) );
+				e.target.dataset.direction = "asc";
+				setArrow(<svg pointerEvents="none" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-sort-numeric-up" viewBox="0 0 16 16">
+				  <path d="M12.438 1.668V7H11.39V2.684h-.051l-1.211.859v-.969l1.262-.906h1.046z"/>
+				  <path fillRule="evenodd" d="M11.36 14.098c-1.137 0-1.708-.657-1.762-1.278h1.004c.058.223.343.45.773.45.824 0 1.164-.829 1.133-1.856h-.059c-.148.39-.57.742-1.261.742-.91 0-1.72-.613-1.72-1.758 0-1.148.848-1.835 1.973-1.835 1.09 0 2.063.636 2.063 2.687 0 1.867-.723 2.848-2.145 2.848zm.062-2.735c.504 0 .933-.336.933-.972 0-.633-.398-1.008-.94-1.008-.52 0-.927.375-.927 1 0 .64.418.98.934.98z"/>
+				  <path d="M4.5 13.5a.5.5 0 0 1-1 0V3.707L2.354 4.854a.5.5 0 1 1-.708-.708l2-1.999.007-.007a.498.498 0 0 1 .7.006l2 2a.5.5 0 1 1-.707.708L4.5 3.707V13.5z"/>
+				</svg>)
+
+			}else{
+				mapToday( array_sort(type,"desc",today) );
+				e.target.dataset.direction = "desc";
+				setArrow(<svg pointerEvents="none" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-sort-numeric-down-alt" viewBox="0 0 16 16">
+						  <path fillRule="evenodd" d="M11.36 7.098c-1.137 0-1.708-.657-1.762-1.278h1.004c.058.223.343.45.773.45.824 0 1.164-.829 1.133-1.856h-.059c-.148.39-.57.742-1.261.742-.91 0-1.72-.613-1.72-1.758 0-1.148.848-1.836 1.973-1.836 1.09 0 2.063.637 2.063 2.688 0 1.867-.723 2.848-2.145 2.848zm.062-2.735c.504 0 .933-.336.933-.972 0-.633-.398-1.008-.94-1.008-.52 0-.927.375-.927 1 0 .64.418.98.934.98z"/>
+						  <path d="M12.438 8.668V14H11.39V9.684h-.051l-1.211.859v-.969l1.262-.906h1.046zM4.5 2.5a.5.5 0 0 0-1 0v9.793l-1.146-1.147a.5.5 0 0 0-.708.708l2 1.999.007.007a.497.497 0 0 0 .7-.006l2-2a.5.5 0 0 0-.707-.708L4.5 12.293V2.5z"/>
+						</svg>)
+			}
+			
+			//remove active class
+			 document.querySelectorAll('.sort-active')[0].className ="es-history-sort";
+			//add active class
+			e.target.classList.add("sort-active");
+		
+	}
+
 
 
 	
@@ -203,11 +285,11 @@ function TodayEvents(prop){
 						<thead>
 							<tr>
 								<th>Event Id</th>
-								<th>Status</th>
+								<th className="es-history-sort" data-sort="status" data-direction="desc" onClick = {sort_click}>Status {arrow}</th>
 								<th>League</th>
 								<th>Home</th>
 								<th>Away</th>
-								<th>Total bet WGR</th>
+								<th className="es-history-sort sort-active" data-sort="bet" data-direction="desc" onClick = {sort_click}>Total bet WGR {arrow}</th>
 								<th>Total bet USD</th>
 							</tr>
 						</thead>
